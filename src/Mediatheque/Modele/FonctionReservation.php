93db401faf69908ee1628b628e836f9772cb1bbe
DavidDeAlmeida <david.de.almeida@outlook.fr>
2018-05-22 23:34:18 +0200

    Add files via upload

--- src/Mediatheque/Modele/FonctionReservation.php
+++ src/Mediatheque/Modele/FonctionReservation.php
@@ -2,7 +2,7 @@
 function Reservation($idR){
 	include('../Constante/ConnexionBDD.php');
 	$idM = $_SESSION['identifiant'];
-	$req = "SELECT ID_RESERV FROM reservation WHERE ID_MEMBRE='".$idM."'";
+	$req = "SELECT ID_RESERV FROM reservation WHERE ID_MEMBRE='".$idM."' AND DATE_RETOUR IS NULL";
 	$resultatEM = $mysqli->query($req);
 	$nbrEmprunts = $resultatEM->num_rows;
 	if($nbrEmprunts < 5){
@@ -12,6 +12,11 @@ function Reservation($idR){
 		if($nbrExemp['NBR_EXEMP'] > 0){
 			$req = "INSERT INTO reservation VALUES (DEFAULT,'".$idM."',".$idR.",'".date('Y-m-d')."',NULL)";
 			$mysqli->query($req);
+
+			$req2 = "SELECT CAUTION FROM ressource WHERE ID_RESSOURCE = ".$idR;
+			$result2 = $mysqli->query($req2);
+			$ligne = mysqli_fetch_array($result2);
+
 			return 1;
 		}
 		else{
@@ -21,5 +26,26 @@ function Reservation($idR){
 	else{
 		return 3;
 	}
+}
+
+function caution($idR){
+	include('../Constante/ConnexionBDD.php');
+	$req = "SELECT CAUTION FROM ressource WHERE ID_RESSOURCE = ".$idR;
+	$result = $mysqli->query($req);
+	$ligne = mysqli_fetch_array($result);
+	return $ligne['CAUTION'];
+}
+
+function RetirerSolde($caution,$idMembre)
+{
+	include('../Constante/ConnexionBDD.php');
+
+	$req1 = "Select SOLDE from utilisateur where IDENTIFIANT ='".$idMembre."'";
+	$result = $mysqli->query($req1);
+	$ligne = mysqli_fetch_array($result);
+	//echo $req1;
+	$nouveauSolde =  $ligne['SOLDE'] - $caution;
+	$req2 ="UPDATE utilisateur SET SOLDE=".$nouveauSolde." WHERE IDENTIFIANT ='".$idMembre."'";
+	$mysqli->query($req2);
 }
 ?>
