bb4303249a9e6bacb73e925f9a99fd4b3ef063cd
DavidDeAlmeida <david.de.almeida@outlook.fr>
2018-05-22 22:03:10 +0200

    Projet GLA

---
+++ src/Mediatheque/Modele/FonctionReservation.php
@@ -0,0 +1,25 @@
+﻿<?php
+function Reservation($idR){
+	include('../Constante/ConnexionBDD.php');
+	$idM = $_SESSION['identifiant'];
+	$req = "SELECT ID_RESERV FROM reservation WHERE ID_MEMBRE='".$idM."'";
+	$resultatEM = $mysqli->query($req);
+	$nbrEmprunts = $resultatEM->num_rows;
+	if($nbrEmprunts < 5){
+		$req = "SELECT NBR_EXEMP FROM ressource WHERE ID_RESSOURCE=".$idR;
+		$resultatEX = $mysqli->query($req);
+		$nbrExemp = mysqli_fetch_array($resultatEX);
+		if($nbrExemp['NBR_EXEMP'] > 0){
+			$req = "INSERT INTO reservation VALUES (DEFAULT,'".$idM."',".$idR.",'".date('Y-m-d')."',NULL)";
+			$mysqli->query($req);
+			return 1;
+		}
+		else{
+			return 2;
+		}
+	}
+	else{
+		return 3;
+	}
+}
+?>
---
+++ src/Mediatheque/Vue/VueReservation.php
@@ -0,0 +1,12 @@
+﻿<?php
+if($Confirmation == 1){
+	echo "Votre réservation a bien été enregistrée.";
+}
+else if($Confirmation == 2){
+	echo "Il n'y a plus d'exemplaires disponibles de cette ressource.";
+}
+else{
+	echo "Vous avez déjà réserver 5 ressources.";
+}
+?>
+<a href="../Controleur/Home.php"><button>Retour à l'accueil</button></a>
